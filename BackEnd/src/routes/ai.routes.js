3d8c4497845e5418826cedbce7b11ec73acf5989
Aman Hussain Khan <amanhussainkhan3@gmail.com>
2025-04-07 11:01:53 +0530

    chnage

--- BackEnd/src/routes/ai.routes.js
+++ BackEnd/src/routes/ai.routes.js
@@ -5,6 +5,9 @@ const router = express.Router();
 
 
 router.post("/get-review", aiController.getReview)
+router.get("/",(req,res)=>{
+  res.send({message:"api is fine..."})
+})
 
 
 module.exports = router;    
